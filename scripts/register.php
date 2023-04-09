e460095af5bdb3b1a1d60f5f8d9f90e59d86c659
xray <sorsorsersar@gmail.com>
2023-04-09 17:17:11 +0300

    registration

---
+++ scripts/register.php
@@ -0,0 +1,29 @@
+<?php
+include_once("db.php");
+
+$u = $_POST['username'];
+$n = $_POST['nick'];
+$p = $_POST['psw'];
+
+$sql = "Insert into `users` (`login`,`nickname`, `password`) values
+ ('$u','$n', '$p')";
+
+ if ( mysqli_query($link,$sql) == 0 )
+    {
+    echo "<script>
+    alert('Ошибка! Пользователь не добавлен! Возможно такой логин уже занят. Попробуйте еще раз');
+          </script>";
+    echo "<script>
+            window.location='../registration.html';
+          </script>";
+    }
+    else
+    {
+    echo"<script>
+          alert('Вы зарегистрированы');
+        </script>";
+    echo "<script>
+        window.location='../index.html';
+          </script>";
+    }
+?>
